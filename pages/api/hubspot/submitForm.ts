987e78081896f63a0e29db402c5e8b7bc75ca357
Matthew Lewis <matthew.a.lewis7@gmail.com>
2022-12-20 09:20:26 -0500

    Update submitForm.ts
    Fixed error message leak.

--- pages/api/hubspot/submitForm.ts
+++ pages/api/hubspot/submitForm.ts
@@ -53,6 +53,6 @@ export default async function handler(
     );
     res.status(200).json({ response: hubspotResponse.data });
   } catch (e) {
-    res.status(500).send(e);
+    res.status(500).send('Error submitting Hubspot form);
   }
 }
